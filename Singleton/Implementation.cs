d8b418fd39ba437e10b3296ea7e5ec44202abb0b
Ramazan Karademir <karademir.ramazan@gmail.com>
2022-03-20 14:00:16 +0300

    Add project files.

---
+++ Singleton/Implementation.cs
@@ -0,0 +1,38 @@
+﻿namespace Singleton
+{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class Logger
+    {
+        //private static Logger? _instance;
+
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(()=> new Logger()); 
+
+        public static Logger Instance
+        {
+            get
+            {
+                return _lazyLogger.Value;
+                //if (_instance == null)
+                //{
+                //    _instance = new Logger();
+                //}
+                //return _instance;
+            }
+        }
+
+        protected Logger()
+        {
+            Console.WriteLine("...Creating Instance...");
+        }
+
+        /// <summary>
+        /// SingletonOperation
+        /// </summary>
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to Log: {message}");
+        }
+    }
+}
