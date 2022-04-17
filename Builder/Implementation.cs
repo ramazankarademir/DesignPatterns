43f93f5defb43fe357aa0ddcd93e01f47d9af276
Ramazan Karademir <karademir.ramazan@gmail.com>
2022-04-17 16:32:46 +0300

    Builder pattern implemented.

--- Builder/Implementation.cs
+++ Builder/Implementation.cs
@@ -1,4 +1,114 @@
-﻿namespace BuilderPattern
-{
+﻿using System.Text;
 
+namespace BuilderPattern
+{
+    /// <summary>
+    /// Product
+    /// </summary>
+    public class Car
+    {
+        private readonly List<string> _parts = new();
+        private readonly string _carType;
+
+        public Car(string carType)
+        {
+            _carType = carType;
+        }
+
+        public void AddPart(string part)
+        {
+            _parts.Add(part);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var part in _parts)
+            {
+                sb.Append($"Car of Type {_carType} has part {part}. ");
+            }
+            return sb.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Builder
+    /// </summary>
+    public abstract class CarBuilder
+    {
+        public Car Car { get; private set; }
+
+        public CarBuilder(string carType)
+        {
+            Car = new Car(carType);
+        }
+
+        public abstract void BuildEngine();
+        public abstract void BuildFrame();
+
+    }
+
+    /// <summary>
+    /// ConcreteBuilder1 class
+    /// </summary>
+    public class MiniBuilder : CarBuilder
+    {
+        public MiniBuilder():base("Mini")
+        {
+
+        }
+        public override void BuildEngine()
+        {
+            Car.AddPart("'not a V8'");
+        }
+
+        public override void BuildFrame()
+        {
+            Car.AddPart("'3-door with stripes'");
+        }
+    }
+
+
+    /// <summary>
+    /// ConcreteBuilder2 class
+    /// </summary>
+    public class BMWBuilder : CarBuilder
+    {
+        public BMWBuilder() : base("BMW")
+        {
+
+        }
+        public override void BuildEngine()
+        {
+            Car.AddPart("'a fancy V8 engine'");
+        }
+
+        public override void BuildFrame()
+        {
+            Car.AddPart("'5-door with metalic finish'");
+        }
+    }
+
+    /// <summary>
+    /// Director
+    /// </summary>
+    public class Garage
+    {
+        private CarBuilder? _builder;
+        public Garage()
+        {
+        }
+        public void Construct(CarBuilder builder)
+        {
+            _builder = builder;
+
+            _builder.BuildEngine();
+            _builder.BuildFrame();
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(_builder?.Car.ToString());
+        }
+    }
 }
--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.Title = "Builder";
+﻿using BuilderPattern;
+
+Console.Title = "Builder";
+
+var garage = new Garage();
+
+var miniBuilder = new MiniBuilder();
+var bmwBuilder = new BMWBuilder();
+
+
+garage.Construct(miniBuilder);
+garage.Show();
+
+garage.Construct(bmwBuilder);
+garage.Show();
+
+Console.ReadLine();
