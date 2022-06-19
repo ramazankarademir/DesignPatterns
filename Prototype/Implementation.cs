735057ead8bd76d810c38592e523c9e215a94667
Ramazan Karademir <karademir.ramazan@gmail.com>
2022-06-19 13:32:53 +0300

    Prototype pattern implemented

--- Prototype/Implementation.cs
+++ Prototype/Implementation.cs
@@ -1,4 +1,64 @@
-﻿namespace Prototype
-{
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization.Formatters.Binary;
 
+namespace Prototype
+{
+    /// <summary>
+    /// Prototype
+    /// </summary>
+
+    [Serializable]
+    public abstract class Person
+    {
+        public abstract string Name { get; set; }
+        public abstract Person Clone(bool deepClone);
+    }
+
+    /// <summary>
+    /// ConcretePrototype1
+    /// </summary>
+    [Serializable]
+    public class Manager : Person
+    {
+        public override string Name { get; set; }
+
+        public Manager(string name)
+        {
+            Name = name;
+        }
+        public override Person Clone(bool deepClone = false)
+        {
+            if (deepClone)
+            {
+                var objectAsJson = JsonConvert.SerializeObject(this);
+                return JsonConvert.DeserializeObject<Manager>(objectAsJson);
+            }
+            return (Person)MemberwiseClone();
+        }
+    }
+
+    /// <summary>
+    /// ConcretePrototype2
+    /// </summary>
+    [Serializable]
+    public class Employee : Person
+    {
+        public Manager Manager { get; set; }    
+        public override string Name { get; set; }
+
+        public Employee(string name, Manager manager)
+        {
+            Name = name;
+            Manager = manager;
+        }
+        public override Person Clone(bool deepClone = false)
+        {
+            if (deepClone)
+            {
+                var objectAsJson = JsonConvert.SerializeObject(this);
+                return JsonConvert.DeserializeObject<Employee>(objectAsJson);
+            }
+            return (Person)MemberwiseClone();
+        }
+    }
 }
--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.Title = "Prototype";
+﻿using Prototype;
+
+Console.Title = "Prototype";
+
+var manager = new Manager("Cindy");
+var managerClone = (Manager)manager.Clone();
+Console.WriteLine($"Manager was cloned: {managerClone.Name}");
+
+var employee = new Employee("Kevin", managerClone);
+var employeeClone = (Employee)employee.Clone(true);
+Console.WriteLine($"Employee was cloned: {employeeClone.Name}, " +
+    $"with manager {employeeClone.Manager.Name}");
+
+managerClone.Name = "Karen";
+Console.WriteLine($"Employee was cloned: {employeeClone.Name}, " +
+    $"with manager {employeeClone.Manager.Name}");
+
+Console.WriteLine($"managerClone.Name: {managerClone.Name}");
+Console.ReadKey();
