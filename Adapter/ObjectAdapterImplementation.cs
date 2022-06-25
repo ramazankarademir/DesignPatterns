c420328c8084f42fb486d79953d103d8023c9909
Ramazan Karademir <karademir.ramazan@gmail.com>
2022-06-25 20:21:58 +0300

    Adapter Pattern implemented

---
+++ Adapter/ObjectAdapterImplementation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class CityFromExternalSystem
+    {
+        public string Name { get; private set; }
+        public string NickName { get; private set; }
+        public int Inhabitants { get; private set; }
+
+        public CityFromExternalSystem(string  name, string nickName, int inhabitants)
+        {
+            Name = name;
+            NickName = nickName;
+            Inhabitants = inhabitants;
+        }
+    }
+
+    /// <summary>
+    /// Adaptee
+    /// </summary>
+    public class ExternalSystem
+    {
+        public CityFromExternalSystem GetCity()
+        {
+            return new CityFromExternalSystem("Antwerp", "'t Stad", 500000);
+               
+        }
+    }
+
+    public class City 
+    {
+        public string FullName { get; set; }
+        public long Inhabitants { get; set; }
+
+        public City(string fullName, long inhabitants)
+        {
+            FullName = fullName;
+            Inhabitants = inhabitants;
+        }
+    }
+
+    /// <summary>
+    /// Target
+    /// </summary>
+    public interface ICityAdapter
+    {
+        City GetCity();
+    }
+
+    /// <summary>
+    /// Adapter
+    /// </summary>
+    public class CityAdapter : ICityAdapter
+    {
+        public ExternalSystem ExternalSystem { get; private set; } = new();
+        public City GetCity()
+        {
+            var cityFromExternalSystem = ExternalSystem.GetCity();
+
+            return new City(
+                $"{cityFromExternalSystem.Name}-{cityFromExternalSystem.NickName}",
+                cityFromExternalSystem.Inhabitants
+            );
+        }
+    }
+}
--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.Title = "Adapter";
+﻿using Adapter;
+
+Console.Title = "Adapter"; 
+
+ICityAdapter adapter = new CityAdapter();
+
+var city = adapter.GetCity();
+
+Console.WriteLine($"{city.FullName}, {city.Inhabitants}");
+
+
+ClassAdapter.ICityAdapter classAdapter = new ClassAdapter.CityAdapter();
+
+var city2 = classAdapter.GetCity();
+
+Console.WriteLine($"{city2.FullName}, {city2.Inhabitants}");
